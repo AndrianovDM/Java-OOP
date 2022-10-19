88d1560643e9e23e4efed189fcc729257f3e3fec
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-10-20 02:37:12 +0300

    add class Relative

---
+++ Homework3/src/data/Relative.java
@@ -0,0 +1,42 @@
+package Homework3.src.data;
+
+public class Relative {
+    private Human firstHuman;
+    private Human secondHuman;
+    private Relation relation;
+
+    public Relative(Human firstHuman, Human secondHuman, Relation relation) {
+        this.firstHuman = firstHuman;
+        this.secondHuman = secondHuman;
+        this.relation = relation;
+    }
+
+    public Human getFirstHuman() {
+        return firstHuman;
+    }
+
+    public  void setFirstHuman(Human firstHuman) {
+        this.firstHuman = firstHuman;
+    }
+
+    public Human getSecondHuman() {
+        return secondHuman;
+    }
+
+    public void setSecondHuman(Human secondHuman) {
+        this.secondHuman = secondHuman;
+    }
+
+    public Relation getRelation() {
+        return relation;
+    }
+
+    public void setRelation(Relation relation) {
+        this.relation = relation;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s of %s", firstHuman, relation, secondHuman);
+        }
+}
