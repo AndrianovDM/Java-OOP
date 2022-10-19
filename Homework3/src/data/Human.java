86390e25634c38f6ac17a13a4ba01eee63505e91
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-10-20 01:53:58 +0300

    add class Human

---
+++ Homework3/src/data/Human.java
@@ -0,0 +1,63 @@
+package Homework3.src.data;
+
+public class Human {
+    private String name;
+    private String patronymic;
+    private String surname;
+    private Integer age;
+    private String gender;
+
+    public Human(String name, String patronymic, String surname, Integer age, String gender) {
+        this.name = name;
+        this.patronymic = patronymic;
+        this. surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public  void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Фамилия: %s, Имя: %s, Отчество: %s, Возраст: %d, Пол: %s.",
+                             surname, name, patronymic, age, gender);
+    }
+}
