a606bbf0280cfd668b0a62e731037809ccaaa7d4
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-10-24 18:23:17 +0300

    add Class Notebook

---
+++ Homework4/src/data/Desktop.java
@@ -0,0 +1,28 @@
+package Homework4.src.data;
+
+public class Desktop extends Computer{
+
+    private Integer power;
+
+    public Desktop(String id, String model, Memory memory, String ssd, Integer power){
+        super(id, model, memory, ssd);
+        this.power = power;
+    }
+
+    public Integer getPower(){
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString(){
+        return "Desktop id: " + getId() + 
+               " ,Model: " + getModel() + 
+               " ,Memory: " + getMemory() + " Gb" +
+               " ,SSD: " + getSsd() + " Gb"+
+               " ,Power: " + getPower();
+    }  
+}
---
+++ Homework4/src/data/NoteBook.java
@@ -0,0 +1,28 @@
+package Homework4.src.data;
+
+public class NoteBook extends Computer{
+
+    private Integer batteryCapacity;
+
+    public NoteBook(String id, String model, Memory memory, String ssd, Integer batteryCapacity){
+        super(id, model, memory, ssd);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public Integer getBatteryCapacity(){
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(Integer batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    @Override
+    public String toString(){
+        return "NoteBook id: " + getId() + 
+               " ,Model: " + getModel() + 
+               " ,Memory: " + getMemory() + " Gb" +
+               " ,SSD: " + getSsd() + " Gb"+
+               " ,BatteryCapacity: " + getBatteryCapacity();
+    }
+}
