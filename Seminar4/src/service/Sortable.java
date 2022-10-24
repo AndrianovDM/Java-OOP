7d168608658c2b4f98fd72841bee9c39b1216391
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-10-24 16:53:14 +0300

    add interface Sortable

---
+++ Seminar4/src/service/Sortable.java
@@ -0,0 +1,8 @@
+package Seminar4.src.service;
+
+import java.util.List;
+import Seminar4.src.data.Computer;
+
+public interface Sortable <T extends Computer>{
+    public List<T> sort(List<T> list);
+}
