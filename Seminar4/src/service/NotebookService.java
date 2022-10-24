f7cc60e6e71baff0854b225aa4254462c9cdedcb
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-10-24 16:51:30 +0300

    add interface Printable

---
+++ Seminar4/src/service/NotebookService.java
@@ -0,0 +1,10 @@
+package Seminar4.src.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class NotebookService {
+    
+}
---
+++ Seminar4/src/service/Printable.java
@@ -0,0 +1,9 @@
+package Seminar4.src.service;
+
+import java.util.List;
+import Seminar4.src.data.Computer;
+
+public interface Printable <T extends Computer>{
+    public void print(List<T> list);
+}
+
