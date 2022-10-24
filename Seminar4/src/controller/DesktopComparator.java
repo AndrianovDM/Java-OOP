c4103f6232b5f092b61616543c14ac4d0ffcd741
Andrianov Dmitry <andrianovdm1997@mail.ru>
2022-10-24 17:55:28 +0300

    add Class DesktopCpmporator

---
+++ Seminar4/src/controller/DesktopComparator.java
@@ -0,0 +1,26 @@
+package Seminar4.src.controller;
+
+import Seminar4.src.data.Desktop;
+import Seminar4.src.service.DesktopService;
+
+import java.util.List;
+
+public class DesktopComparator {
+    private DesktopService desktopService;
+
+    public void DesktopConrtoller(){
+        this.desktopService = new DesktopService();
+    }
+    
+    public List<Desktop> getSortedByIdNotebook(List<Desktop> list) {
+        return desktopService.sort(list);
+    }
+
+    public void printInLogDesktops(List<Desktop> list) {
+        desktopService.print(list);
+    }
+
+    public List<Desktop> getFilteredDesktopList(List<Desktop> list, String field, String value){
+        return desktopService.FindAndWriteValue(list, field, value);
+    }
+}
---
+++ Seminar4/src/controller/NotebookController.java
@@ -0,0 +1,27 @@
+package Seminar4.src.controller;
+
+import Seminar4.src.data.NoteBook;
+import Seminar4.src.service.NotebookService;
+
+import java.util.List;
+
+public class NotebookController {
+    private NotebookService notebookService;
+
+    public void NotebookConrtoller(){
+        this.notebookService = new NotebookService();
+    }
+    
+    public List<NoteBook> getSortedByIdNotebook(List<NoteBook> list) {
+        return notebookService.sort(list);
+    }
+
+    public void printInLogNotebooks(List<NoteBook> list) {
+        notebookService.print(list);
+    }
+
+    public List<NoteBook> getFilteredNotebookList(List<NoteBook> list, String field, String value){
+        return notebookService.FindAndWriteValue(list, field, value);
+    }
+}
+
